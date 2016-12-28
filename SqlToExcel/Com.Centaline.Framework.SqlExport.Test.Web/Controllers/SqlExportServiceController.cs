4e4e861655e9b77ca757fc77bd20f312e93e52bd
wanglin36 <wanglin36@CENTALINE.COM>
2016-12-28 16:19:43 +0800

    remove unecessary dll

--- SqlToExcel/Com.Centaline.Framework.SqlExport.Test.Web/Controllers/SqlExportServiceController.cs
+++ SqlToExcel/Com.Centaline.Framework.SqlExport.Test.Web/Controllers/SqlExportServiceController.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Data;
 using System.IO;
-using System.Web;
 using System.Web.Mvc;
 using Com.Centaline.Framework.Kernel.Json.Interface;
 using Com.Centaline.Framework.QuickQuery.Utils;
@@ -10,11 +9,8 @@ using Spring.Objects.Factory.Attributes;
 using Spring.Objects.Factory.Support;
 using SqlExport.Interface;
 using SqlToExcel.Module.Common;
-using org.in2bits.MyXls;
-using System.Reactive.Linq;
 using System.Xml.Linq;
 using SqlToExcel.Module.Mail;
-using System.Runtime.Remoting.Contexts;
 
 namespace SqlToExcel.Controllers
 {
@@ -105,13 +101,7 @@ namespace SqlToExcel.Controllers
             result = "用户没有权限执行导出功能";
             return ObjectContainer.Instance.GetObject<IJsonSerializer>().ToJson(result); 
         }
-        public static void Repeat(Action action, TimeSpan interval)
-        {
-            Observable.Defer(action.ToAsync())
-                      .DelaySubscription(interval)
-                      .Repeat().Subscribe();
-        }
-
+      
 
         public string SendMail()
         {
@@ -132,7 +122,7 @@ namespace SqlToExcel.Controllers
             {
                 DateTime dateTime = DateTime.Parse((string) xele.Attribute("content"));
                 TimeSpan span = DateTime.Now - dateTime;
-                if (span.TotalMinutes < 1)
+                if (span.TotalMinutes < 30)
                 {
                     //Response.Redirect("/Home/Index");
                     return this.Content("<script>alert('30分钟内不能重复导出到excel!')</script>");
@@ -183,107 +173,6 @@ namespace SqlToExcel.Controllers
             return File(ms, "application/vnd.ms-excel", strdate + "Excel.xls");
         }
 
-        /// <summary>
-        /// MyXls组件保存网页Excel，用于webform的Page.Response
-        /// </summary>
-        public void ExportExcelForPercentForWeb(string sheetName, string xlsname)
-        {
-            XlsDocument xls = new XlsDocument();
-            Worksheet sheet = xls.Workbook.Worksheets.Add(sheetName);
-            try
-            {
-                string sql = "SELECT  [UserID],[UserCode],[UserName],[ParentID],[Position],[Mobile],[Email],[Levels],[AttentionTime]FROM Users WHERE CreateStatus = 1 AND(AttentionState = 1); ";
-                DataTable table = ObjectContainer.Instance.GetObject<ISqlExportService>().GetDataTableFromSql(Request, sql);
-
-
-                if (table == null || table.Rows.Count == 0) { return; }
-                //XlsDocument xls = new XlsDocument();
-                //Worksheet sheet = xls.Workbook.Worksheets.Add(sheetName);
-
-                //填充表头  
-                foreach (DataColumn col in table.Columns)
-                {
-                    sheet.Cells.Add(1, col.Ordinal + 1, col.ColumnName);
-                }
-
-                //填充内容  
-                for (int i = 0; i < table.Rows.Count; i++)
-                {
-                    for (int j = 0; j < table.Columns.Count; j++)
-                    {
-                        sheet.Cells.Add(i + 2, j + 1, table.Rows[i][j].ToString());
-                    }
-                }
-
-                //保存  
-                //xls.FileName = xlsname;
-                //xls.Save();
-
-                #region 客户端保存
-                using (MemoryStream ms = new MemoryStream())
-                {
-                    xls.Save(ms);
-                    ms.Flush();
-                    ms.Position = 0;
-                    sheet = null;
-                    xls = null;
-                    HttpResponse response = System.Web.HttpContext.Current.Response;
-                    response.Clear();
-
-                    response.Charset = "UTF-8";
-                    response.ContentType = "application/vnd.ms-excel";//"application/vnd.ms-excel";
-                    System.Web.HttpContext.Current.Response.AddHeader("Content-Disposition", string.Format("attachment; filename=" + xlsname));
-                    //System.Web.HttpContext.Current.Response.WriteFile(fi.FullName);
-                    byte[] data = ms.ToArray();
-                    System.Web.HttpContext.Current.Response.BinaryWrite(data);
-                   
-                }
-                #endregion
-                //xls = null;
-            }
-            catch (Exception ex)
-            {
-            }
-            finally
-            {
-                sheet = null;
-                xls = null;
-            }
-        }
-
-        /// <summary>
-        /// MyXls组件保存本地Excel，用于webform的Page.Response
-        /// </summary>
-        public  void ExportExcelForPercent(string sheetName, string xlsname)
-        {
-            string sql = "SELECT  [UserID],[UserCode],[UserName],[ParentID],[Position],[Mobile],[Email],[Levels],[AttentionTime]FROM Users WHERE CreateStatus = 1 AND(AttentionState = 1); ";
-            DataTable table = ObjectContainer.Instance.GetObject<ISqlExportService>().GetDataTableFromSql(Request, sql);
-          
-
-            if (table == null || table.Rows.Count == 0) { return; }
-            XlsDocument xls = new XlsDocument();
-            Worksheet sheet = xls.Workbook.Worksheets.Add(sheetName);
-
-            //填充表头  
-            foreach (DataColumn col in table.Columns)
-            {
-                sheet.Cells.Add(1, col.Ordinal + 1, col.ColumnName);
-            }
-
-            //填充内容  
-            for (int i = 0; i < table.Rows.Count; i++)
-            {
-                for (int j = 0; j < table.Columns.Count; j++)
-                {
-                    sheet.Cells.Add(i + 2, j + 1, table.Rows[i][j].ToString());
-                }
-            }
-
-            //保存  
-            xls.FileName = xlsname;
-            xls.Save();
-            xls = null;
-        }
-
+     
     }
 }
