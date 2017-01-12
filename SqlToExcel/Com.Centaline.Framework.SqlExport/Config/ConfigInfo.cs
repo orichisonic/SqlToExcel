0c392a8621db65fed381f50d56e66a581fe87abd
wanglin36 <wanglin36@CENTALINE.COM>
2017-01-12 16:10:31 +0800

    amendy

---
+++ SqlToExcel/Com.Centaline.Framework.SqlExport/Config/ConfigInfo.cs
@@ -0,0 +1,58 @@
+﻿namespace SqlExport.Config
+{
+    public class ConfigInfo
+    {
+
+
+        public static string ServerName
+        {
+            get
+            {
+                 return System.Configuration.ConfigurationManager.AppSettings["ServerName"];
+            }
+        }
+
+        public static string UserName
+        {
+            get
+            {
+                return System.Configuration.ConfigurationManager.AppSettings["UserName"];
+            }
+        }
+
+        public static string UserPwd
+        {
+            get
+            {
+                return System.Configuration.ConfigurationManager.AppSettings["UserPwd"];
+            }
+        }
+
+
+
+        public static string DataBase
+        {
+            get
+            {
+                return System.Configuration.ConfigurationManager.AppSettings["DataBase"];
+            }
+        }
+
+        public static string ValidateType
+        {
+            get
+            {
+                return System.Configuration.ConfigurationManager.AppSettings["ValidateType"];
+            }
+        }
+
+
+        public static string ProviderName
+        {
+            get
+            {
+                return System.Configuration.ConfigurationManager.AppSettings["ProviderName"];
+            }
+        }
+    }
+}
---
+++ SqlToExcel/Com.Centaline.Framework.SqlExport/Entity/PageResult.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Com.Centaline.Framework.Kernel.DTO.Entity;
+
+namespace SqlExport.Entity
+{
+    public class PageResult<TData>: Result<TData>
+    {
+        public PageInfo PageInfo { get; set; }
+
+        public PageResult()
+        {
+            PageInfo=new PageInfo(0,0,0);
+        }
+    }
+}
